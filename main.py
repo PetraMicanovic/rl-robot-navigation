f79a32bf539ba52cb52890aa17224a6880af061b
PetraMicanovic <micanovic.petra@yahoo.com>
2026-05-31 15:19:03 +0200

    Validate evaluation module integration

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from training.train import train
+from evaluation.evaluate import evaluate
 
 def main():
     train(n_dynamic_obstacles=0, obstacle_speed=1.0)
+    evaluate(
+        model_path="models/ppo_robot_nav_obs0_spd1.0",
+        n_dynamic_obstacles=0,
+        obstacle_speed=1.0
+    )
 
 if __name__ == "__main__":
     main()
