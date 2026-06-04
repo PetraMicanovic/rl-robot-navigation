0833cf5eba760f0e6ddac1cc91a4521b807e50cb
PetraMicanovic <micanovic.petra@yahoo.com>
2026-06-04 09:02:52 +0200

    Integrate E3 experiment into main entry point

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from experiments.experiment_e1 import run_e1
 from experiments.experiment_e2 import run_e2
+from experiments.experiment_e3 import run_e3
 
 if __name__ == "__main__":
     run_e1()
     run_e2()
+    run_e3()
