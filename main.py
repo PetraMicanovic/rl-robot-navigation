709bf7e1892d23833677a112e3b1be88f9a32465
PetraMicanovic <micanovic.petra@yahoo.com>
2026-06-05 10:04:38 +0200

    Add Experiment E4 execution

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from experiments.experiment_e1 import run_e1
 from experiments.experiment_e2 import run_e2
 from experiments.experiment_e3 import run_e3
+from experiments.experiment_e4 import run_e4
 
 if __name__ == "__main__":
     run_e1()
     run_e2()
     run_e3()
+    run_e4()
