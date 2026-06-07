4a308b086351a6b3a532c98ef3ee1d7a72c73be6
PetraMicanovic <micanovic.petra@yahoo.com>
2026-06-07 09:44:57 +0200

    Add automatic visualization generation after E1-E4

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from experiments.experiment_e1 import run_e1
 from experiments.experiment_e2 import run_e2
 from experiments.experiment_e3 import run_e3
 from experiments.experiment_e4 import run_e4
+from visualization.visualize import run_visualization
 
 if __name__ == "__main__":
     run_e1()
     run_e2()
     run_e3()
     run_e4()
+    run_visualization()
